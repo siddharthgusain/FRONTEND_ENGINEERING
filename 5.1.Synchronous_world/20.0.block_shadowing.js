c503fcc617f159bf5a9a7d451f0597d3595fd7de
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-01 21:47:19 +0530

    illegal shaowding

--- 5.1.Synchronous_world/20.0.block_shadowing.js
+++ 5.1.Synchronous_world/20.0.block_shadowing.js
@@ -7,17 +7,17 @@ let illegal_shadowing = 1;
 // block
 { 
     // compound statement
-    // var illegal_shadowing = 1; // not allowed
-    var a = 10;
-    let b = 200;
-    const c = 211;
+    // var illegal_shadowing = 1; // not allowed in case of let
+    var a = 10;  // it "shadows" outer "a" and modifies it as both are pointing to same location
+    let b = 200; // not shadows as both "b" variabvle are different in memory because "let" works this way
+    const c = 211; // not shadows as both "c" variabvle are different in memory because "const" works this way
     console.log(a);
     console.log(b);
     console.log(c);
 
 }
 
-console.log(a);
-console.log(b); 
-console.log(c);
+console.log(a); // got shadowed because of var declaration
+console.log(b); // not shadowed because let declaration
+console.log(c); // not shadowed
 
