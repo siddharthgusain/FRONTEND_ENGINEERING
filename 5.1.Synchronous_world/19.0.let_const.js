22665dd1db2bc0583fcd978b9fd43c121cab08e1
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-01 21:35:19 +0530

    let and const

--- 5.1.Synchronous_world/19.0.let_const.js
+++ 5.1.Synchronous_world/19.0.let_const.js
@@ -5,8 +5,9 @@ const c = 20;
 var b = 100;
 // let and const variable can't be referenced before they are initialized
 // let and const have different memory space they are not attached to window object
+// we can't redeclare let and const variable but var can be redeclared
 // c = 22; // constant variable
 console.log(a);
 
 
-// Temporal deadzone :- Time between the let and const declaration and initilization is deadzone
+// Temporal deadzone :- Time between the let and const declaration are hoisted and gets initilization is deadzone
