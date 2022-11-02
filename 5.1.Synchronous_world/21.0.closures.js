8ad5de29a3f854a581df94a190af994fe6bd4c29
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-02 13:33:04 +0530

    closure basic

--- 5.1.Synchronous_world/21.0.closures.js
+++ 5.1.Synchronous_world/21.0.closures.js
@@ -23,10 +23,13 @@ function outer()
     // y is garbage collected and x is present inside closure because it is being used 
     // inside inner function
     return function inner(){
-        console.log(x);
+        console.log(x); // "x" is only present in closure of inner not "y"
     }
 }
 
 var res = outer();
 
 res(); // prints 10
+
+// Function bundled with its lexical environment is known as a closure.
+// THats why closure can take up lot of RAM space , thats why its important to understand closure
