ed79d6a959cd1d7b935e72c1baf581fb3d7d72bf
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-02 17:05:39 +0530

    functions

--- 5.1.Synchronous_world/17.0.functions.js
+++ 5.1.Synchronous_world/17.0.functions.js
@@ -1,34 +1,35 @@
 // function statement aka function declaration
 function_statement(); // perfectly fine
-function function_statement(){
+function function_statement() {
     console.log("I am function statement");
 }
 
 // function expression
 // note:- func_exp(); // calling here will lead to error as func_exp is hoisted , at starting it is undefined
-let func_exp = function function_exp(){
+let func_exp = function function_exp() {
     console.log("Function expression");
 }
 func_exp();
 
 // anonymous function( without name)
-var anony_func = function (){
+var anony_func = function () {
     console.log("Anonymous function");
 }
 anony_func();
 
 // named function expression
-var name_func = function named(){
+var name_func = function named() {
     console.log("named function expression");
 }
 
 name_func(); // perfectly fine
-// named(); // error
+// named(); // referrence error
 
 // first class function // first class citizens
 
 /* -> A programming language is said to have First-class functions when functions 
-in that language are treated like any other variable. 
+in that language are treated like any other variable and can be passed ana returned from another
+function
 
 For example, in such a language, a function can be passed as an argument to other functions, 
 can be returned by another function and can be assigned as a value to a variable.
@@ -49,6 +50,6 @@ var arrow = (num) => {
 arrow(10);
 
 // IIFE(immedieately invoked function express)
-(function (){
+(function () {
     console.log("IIFE");
 })();
