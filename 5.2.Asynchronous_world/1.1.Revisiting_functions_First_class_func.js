0c1b0cd133ff339be6aa4fc5487fea19eb17bddf
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-03 16:13:35 +0530

    callbacks and higerh order functions

--- 5.2.Asynchronous_world/1.1.Revisiting_functions_First_class_func.js
+++ 5.2.Asynchronous_world/1.1.Revisiting_functions_First_class_func.js
@@ -6,12 +6,12 @@ function higherOrder(params) { // higher order function
     console.log(params);
 }
 
-function callBack(){ // callback
+function callBack() { // callback
     console.log("hello")
 }
 
 higherOrder(callBack);
 
 
-// Higher order function are functions which takes another function as arguemnt or return 
-// Callbacks are function that are passed as arguemnt to higher order functions
+// Higher order function are functions which takes another function as arguemnt or return
+// Callbacks are function that are passed as arguement to higher order functions
