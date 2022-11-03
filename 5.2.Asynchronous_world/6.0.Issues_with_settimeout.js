ecef969cb6e87e0f3274ab1c45375d981aef644e
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-03 22:18:27 +0530

    SET TIMEOUT ISSUES

---
+++ 5.2.Asynchronous_world/6.0.Issues_with_settimeout.js
@@ -0,0 +1,12 @@
+setTimeout(function () {
+    console.log("I am called immediately?");
+}, 0);
+
+/* 
+This doesnt gurantee that settimeout will run immedietaly 
+It all depends on state of call stack if it is empty or not
+Settimeout gives the minimum time after which callback will be 
+executed in ideal condition.
+*/
+
+
