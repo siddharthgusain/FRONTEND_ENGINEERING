eacb4fbc245c3ce51e827c5ec9fec50041d0d180
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-03 10:03:46 +0530

    object basic

--- 5.1.Synchronous_world/23.0.Objects_without_using_class.js
+++ 5.1.Synchronous_world/23.0.Objects_without_using_class.js
@@ -11,14 +11,18 @@ var a2 = {
     age: 22,
     bio: function () {
         console.log(this);
+    },
+    introduceSelf: function () {
+        console.log(`My name is ${this.name.first}`)
     }
 };
 
 a2.bio();
+a2.introduceSelf();
 window.console.log(a2.name.first); // one way to access
 window.console.log(a2['name']['first']) // second way using indexing
 
-var a2 = {
+var a3 = {
     name: {
         first: "sid",
         last: "gusain"
@@ -28,3 +32,31 @@ var a2 = {
         console.log(this);
     }
 };
+
+// creating obj with simple function
+function createPerson(nameParam) {
+    const obj = {};
+    obj.name = nameParam;
+    obj.introduceSelf = function () {
+
+        console.log(`Hi i am ${this.name}`);
+    }
+
+    return obj;
+}
+
+const sid = createPerson("sid");
+
+sid.introduceSelf();
+
+// object creation better way with constructor
+
+function Person(nameParam) {
+    this.name = nameParam;
+    this.introduceSelf = function () {
+        console.log(`Hi i am ${this.name}`);
+    };
+}
+
+const sidWithConstructor = new Person("sid");
+sidWithConstructor.introduceSelf();
