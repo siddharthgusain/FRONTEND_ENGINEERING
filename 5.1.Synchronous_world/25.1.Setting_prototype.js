98f7da28a9df20d7fcdf421d319fef9c6b2fdbe4
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-03 10:48:01 +0530

    Object.create and Object.assign

---
+++ 5.1.Synchronous_world/25.1.Setting_prototype.js
@@ -0,0 +1,34 @@
+const personPrototype = {
+    greet() {
+        console.log("hello");
+    },
+};
+
+// using Object.create
+const sid = Object.create(personPrototype);
+sid.greet();
+
+// using constructor
+
+const personPrototype1 = {
+    greet() {
+        console.log(`hello ${this.name}`);
+    },
+};
+
+function Person(nameParam) {
+    this.name = nameParam;
+}
+
+Object.assign(Person.prototype, personPrototype1);
+
+const sid1 = new Person("sid with assign");
+sid1.greet(); // greet is available as Person.prototype contains it (assign does that)
+
+/* 
+Properties that are defined directly in the object, like name here, are called own properties,
+ and you can check whether a property is an own property using the static Object.hasOwn() method
+*/
+
+console.log(Object.hasOwn(sid1, "name")); // true as name is own property of sid1
+console.log(Object.hasOwn(sid1, "greet")) // greet is not own property
