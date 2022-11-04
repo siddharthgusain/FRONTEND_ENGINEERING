a2a55c20f2b46148e4f0cfdf43b79e7889e00f8a
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-04 13:54:25 +0530

    asynchronous depth

---
+++ 5.2.Asynchronous_world/8.1.Long_running_syn_function.js
@@ -0,0 +1,38 @@
+const MAX_PRIME = 1000000;
+
+function isPrime(n) {
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+    return n > 1;
+}
+
+const random = (max) => Math.floor(Math.random() * max);
+
+function generatePrimes(quota) {
+    const primes = [];
+    while (primes.length < quota) {
+        const candidate = random(MAX_PRIME);
+        if (isPrime(candidate)) {
+            primes.push(candidate);
+        }
+    }
+    return primes;
+}
+
+const quota = document.querySelector('#quota');
+const output = document.querySelector('#output');
+
+document.querySelector('#generate').addEventListener('click', () => {
+    const primes = generatePrimes(quota.value); // this call takes a long time in callstack so UI is unresponsive for that time
+    output.textContent = `Finished generating ${quota.value} primes!`;
+});
+
+document.querySelector('#reload').addEventListener('click', () => {
+    document.location.reload();
+});
+
+
+// so Generate prime is blocking th main thread as it is synchronous
