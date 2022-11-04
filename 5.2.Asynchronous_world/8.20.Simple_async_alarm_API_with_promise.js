bc02abdfc23938239425d5ea8ce5a8499c553315
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-04 16:58:30 +0530

    implmenting promise based API

---
+++ 5.2.Asynchronous_world/8.20.Simple_async_alarm_API_with_promise.js
@@ -0,0 +1,34 @@
+const personName = document.getElementById("name");
+const delay = document.getElementById("delay");
+const output = document.getElementById("output");
+const clickButton = document.getElementById("clickButton");
+
+//
+function setAlarm(name, delay) {
+    let promise = new Promise((resolve, reject) => {
+        if (delay < 0)
+            throw new Error("Wrong delay");
+
+        setTimeout(() => {
+            resolve(`Wake up ${name}`);
+        }, delay);
+
+    });
+
+    return promise;
+}
+
+// without async await
+clickButton.addEventListener("click", () => {
+    setAlarm(personName.value, delay.value).then((msg) => {
+        output.innerText = msg;
+    })
+});
+
+//with async await cleaner way of writing code
+clickButton.addEventListener("click", async () => {
+    const msg = await setAlarm(personName.value, delay.value);
+    output.innerText = msg;
+})
+
+
