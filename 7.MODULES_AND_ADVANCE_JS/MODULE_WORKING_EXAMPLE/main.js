820ca37690a642be4553290f073cf16db2e42632
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-06 00:32:25 +0530

    in depth analysis of JS modules

--- 7.MODULES_AND_ADVANCE_JS/MODULE_WORKING_EXAMPLE/main.js
+++ 7.MODULES_AND_ADVANCE_JS/MODULE_WORKING_EXAMPLE/main.js
@@ -1,2 +1,9 @@
 import { exportedFunction } from "./Module.js";
+
+// function exportedFunction() {
+//     console.log("I am not from module");
+// }
+
 exportedFunction();
+
+//NOTE:- We can't redeclare same function that was in module
