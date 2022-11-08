c0ad4419da7f12c145e856a452da6283b8e5c63d
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-08 17:38:37 +0530

    advantage and limitation of event delegation

--- Interview_imp_questions/4.Event_delegation.js
+++ Interview_imp_questions/4.Event_delegation.js
@@ -10,3 +10,16 @@ document.getElementById("category").addEventListener('click', (e) => {
     if (e.target.tagName == "LI")
         window.location.href = "/" + e.target.id;
 }); // attaching event listner to parent rather than tha child elements 
+
+
+
+/*
+1) Improves memory space
+2) Mitigates risk of performance bottle neck
+3) DOM manipulation
+4) When elements get added dynamically, the process of adding events is slow
+
+Limitations:
+1) All the events are"not bubbled up", some events like "blur, focus" are not bubbled up
+2) If e.stopPropogation is used in child, then events are not bubbled up
+*/
