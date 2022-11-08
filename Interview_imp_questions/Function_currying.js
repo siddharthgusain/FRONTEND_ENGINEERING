7fdda3873d6a6a482e9cbda9d22fa4673d2e23e5
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-08 12:56:23 +0530

    function currying

---
+++ Interview_imp_questions/Function_currying.js
@@ -0,0 +1,23 @@
+let multiply = function (x, y) {
+    console.log(x * y);
+}
+
+// currying with bind function
+let multiplyByTwo = multiply.bind(this, 2);
+multiplyByTwo(5); // 2 * 5
+
+let multiplyByThree = multiply.bind(this, 3);
+multiplyByTwo(5);// 3 * 5
+
+// Curry with closures
+let multiply1 = function (x) {
+    return function (y) {
+        console.log(x * y);
+    }
+}
+
+let multiplyByTwo1 = multiply1(2);
+multiplyByTwo1(5); // 2 * 5
+
+let multiplyByThree1 = multiply1(3);
+multiplyByTwo(5);// 3 * 5
