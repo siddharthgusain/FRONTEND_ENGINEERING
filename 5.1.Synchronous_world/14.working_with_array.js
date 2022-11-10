ebb0f44f0bbfe03d5e82f8d34e41252f03fc0002
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-11 01:35:52 +0530

    working with arrays

--- 5.1.Synchronous_world/14.working_with_array.js
+++ 5.1.Synchronous_world/14.working_with_array.js
@@ -1,4 +1,5 @@
-var arr = [1,2,3];
+var arr = [1, 2, 3];
+var arr1 = new Array(1, 2, 3);
 
 console.log(arr);
 console.log(arr[0]);
@@ -6,8 +7,7 @@ console.log(arr[0]);
 // common methods
 console.log(arr.length);
 
-for(var i = 0 ; i < arr.length;i++)
-{
+for (var i = 0; i < arr.length; i++) {
     console.log(arr[i]);
 }
 
@@ -18,21 +18,32 @@ console.log(arr);
 string = arr.join(','); // array to string , takes arguement
 console.log(string);
 
-var arr2 = [1,2,3,4];
+var arr2 = [1, 2, 3, 4];
 var str = arr2.toString(); // array to string , always use comma as default
 
 console.log(str);
 
 // push , pop to remove and add from end
-var arr3 = [1,23,2];
+var arr3 = [1, 23, 2];
 arr3.push(22); // acts as stack
-console.log(arr3); 
+console.log(arr3);
 arr3.pop();
 console.log(arr3);
 
 // shift and unshift to remove and add from start
-var arr4 = [1,2,3,4];
+var arr4 = [1, 2, 3, 4];
 arr4.unshift(2222);
 console.log(arr4);
 arr4.shift();
 console.log(arr4);
+
+arr4.slice(1, 3); //[1,2,3]
+
+
+// -------SPLICE------------
+const myArray = ['1', '2', '3', '4', '5'];
+myArray.splice(1, 3, 'a', 'b', 'c', 'd');
+// myArray is now ["1", "a", "b", "c", "d", "5"]
+// This code started at index one (or where the "2" was),
+// removed 3 elements there, and then inserted all consecutive
+// elements in its place.
