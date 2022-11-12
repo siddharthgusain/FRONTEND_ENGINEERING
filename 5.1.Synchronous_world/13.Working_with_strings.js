504945677dd0b56b0a558adace02ea071b9d6781
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-12 23:56:54 +0530

    working with strings

--- 5.1.Synchronous_world/13.Working_with_strings.js
+++ 5.1.Synchronous_world/13.Working_with_strings.js
@@ -1,4 +1,4 @@
-var st = 'siddharth';
+var st = 'siddharth'; // string literal
 var eg = 'I\'ve baseball';
 
 console.log(st + eg);
@@ -16,10 +16,22 @@ console.log(st1.charAt(0));
 console.log(st1.length);
 console.log(st1.indexOf("ll")); // starting index of "ll" substring
 console.log(st1.indexOf("as")); //return -1
-console.log(st1.slice(1,3)); // el
+console.log(st1.slice(1, 3)); // el
 console.log(st1.slice(2)); // llo
 console.log(st1.toUpperCase()); // temperory change
 console.log(st1); // hello
-console.log(st1.replace("hell","mell")); // temperory change
+console.log(st1.replace("hell", "mell")); // temperory change
 console.log(st1); // mello
+
+// Creating string with String constructor Function
+let stringObject = new String("Siddharth");
+let stringLiteral = "siddharth";
+
+console.log(typeof stringLiteral); // string
+console.log(typeof stringObject); // object
+console.log(stringObject); // String object
+
+
+
+
 
