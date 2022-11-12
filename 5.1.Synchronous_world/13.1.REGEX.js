75cef4a35fc207e3513d9bf98f4c59f1ffcdc477
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-13 00:03:08 +0530

    regular expression basics

---
+++ 5.1.Synchronous_world/13.1.REGEX.js
@@ -0,0 +1,7 @@
+// Regular expressions are patterns used to match character combinations in "strings"
+
+// Regular expression with literals
+const RE = /ab+c/;
+
+// Regular expression with Constructor function of RegExp Object
+const RE1 = new RegExp("ab+c")
