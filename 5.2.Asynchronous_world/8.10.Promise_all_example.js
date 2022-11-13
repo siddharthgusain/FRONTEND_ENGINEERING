a1df28672854bfca87e44e3ebeb51c49417c6afb
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-14 00:30:19 +0530

    promise composition

--- 5.2.Asynchronous_world/8.10.Promise_all_example.js
+++ 5.2.Asynchronous_world/8.10.Promise_all_example.js
@@ -13,4 +13,35 @@ Promise.all([fetchPromise1, fetchPromise2, fetchPromise3])
     });
 
 // Note:- the callback in then() is only called once all the 3 promises are "settled"
+// NOTE:- If any of the promise "REJECT" , then it will abort the other as well
 // Usefull when the API calls are not dependent on each other.
+
+
+//----------------PROMISE.ALLSETTLED-----------------
+/*
+The Promise.allSettled() method takes an iterable of promises as input and returns a single Promise.
+This returned promise fulfills when all of the input's promises settle (including when an empty 
+iterable is passed), with an array of objects that describe the outcome of each promise.
+*/
+
+const promise1 = Promise.resolve(3);
+const promise2 = new Promise((resolve, reject) => setTimeout(reject, 100, 'foo'));
+const promises = [promise1, promise2];
+
+Promise.allSettled(promises)
+    .then((results) => results.forEach((result) => console.log(result.status)));
+
+/*
+The Promise.allSettled() method is one of the promise concurrency methods. 
+Promise.allSettled() is typically used when you have multiple asynchronous tasks 
+that are not dependent on one another to complete successfully,
+"or you'd always like to know the result of each promise"
+*/
+
+//------------ANOTHER EXAMPLE------------
+Promise.allSettled([
+    Promise.resolve(33), // promise1
+    new Promise((resolve) => setTimeout(() => resolve(66), 0)), //promise2
+    99,
+    Promise.reject(new Error("an error")), //promise 3
+]).then((values) => console.log(values));
