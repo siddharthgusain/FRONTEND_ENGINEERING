2f317fbbb6d3a60d04e58ab8a80814ea7feed77b
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-19 03:15:42 +0530

    array working

--- 5.1.Synchronous_world/14.0.working_with_array.js
+++ 5.1.Synchronous_world/14.0.working_with_array.js
@@ -1,5 +1,5 @@
 var arr = [1, 2, 3]; // array literal
-var arr1 = new Array(1, 2, 3); // Cnstructor of Array Object
+var arr1 = new Array(1, 2, 3); // Constructor of Array Object
 
 console.log(arr);
 console.log(arr[0]);
@@ -21,7 +21,7 @@ console.log(string);
 var arr2 = [1, 2, 3, 4];
 var str = arr2.toString(); // array to string , always use comma as default
 
-console.log(str);
+console.log(str); "1,2,3,4,5"
 
 // push , pop to remove and add from end
 var arr3 = [1, 23, 2];
@@ -33,21 +33,25 @@ console.log(arr3);
 // shift and unshift to remove and add from start
 var arr4 = [1, 2, 3, 4];
 arr4.unshift(2222);
-console.log(arr4);
-arr4.shift();
-console.log(arr4);
+console.log(arr4); // [2222,1,2,3,4]
+arr4.shift(); // return 2222
+console.log(arr4); // 
+
+// arr --> [1,2,3,4]
 
 //------SLICE-------------
-arr4.slice(1, 3); //[1,2,3]
+console.log(arr4.slice(1, 3)); //[2,3]
 
 
 // -------SPLICE------------
 const myArray = ['1', '2', '3', '4', '5'];
 myArray.splice(1, 3, 'a', 'b', 'c', 'd');
-// myArray is now ["1", "a", "b", "c", "d", "5"]
-// This code started at index one (or where the "2" was),
-// removed 3 elements there, and then inserted all consecutive
-// elements in its place.
+
+/* myArray is now ["1", "a", "b", "c", "d", "5"]
+This code started at index one (or where the "2" was),
+removed 3 elements there, and then inserted all consecutive
+elements in its place.
+*/
 
 // ----- Flattening array---------
 const multiArr = [1, 2, [3, 4, 5]];
