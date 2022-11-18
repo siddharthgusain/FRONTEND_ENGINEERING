c7766182f1ef4339723d9a140e2c5d553ea704e6
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-19 03:03:53 +0530

    basic data types

--- 5.1.Synchronous_world/9.1.basic_data_types.js
+++ 5.1.Synchronous_world/9.1.basic_data_types.js
@@ -2,6 +2,9 @@
 var num = 10;
 var num1 = 10.888;
 
+// NUll
+var nullData = null;
+
 // Strings
 var st = "hello world";
 
@@ -11,14 +14,14 @@ var bool = true;
 
 // arrays
 
-var arr = [1,2,3];
-var arr1 = ["sid" , "gusain"];
+var arr = [1, 2, 3];
+var arr1 = ["sid", "gusain"];
 
 // Object
 
 var obj = {
-    name : "sid",
-    roll : 20
+    name: "sid",
+    roll: 20
 };
 
 console.log(obj);
