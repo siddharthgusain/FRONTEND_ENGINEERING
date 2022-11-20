23a74084e7518ae145d933d1a1bb7938e8264b5d
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-20 17:42:46 +0530

    call apply bind

--- INTERVIEW_PREPARATION/2.Some_useful_functions/1.call.js
+++ INTERVIEW_PREPARATION/2.Some_useful_functions/1.call.js
@@ -6,7 +6,7 @@ let obj = {
     }
 }
 
-obj.printFullName("I am Siddharth", "I love football");
+obj.printFullName("Hey everyone", "I love football");
 
 // Call Apply bind are explosed to any function we create as they are part of "Function.prototype"
 
@@ -21,8 +21,10 @@ obj.printFullName.call(obj1, "I am Tushar", "I like games");
 
 // can pass any number of argument to call
 // "this" points to first arguement passed to call function
-// call function is used to call a function which may not be part of object , so you borrow the function from other object
-
+/*
+call function is used to call a function which may not be part of object , 
+so you borrow the function from other object
+*/
 
 function greeting() {
     console.log(this);
@@ -35,6 +37,6 @@ let obj2 = {
 }
 
 // I want to call greetings() but with obj2 , how to do it? 
-// We can use apply
+// We can use call
 
 greeting.call(obj2) // here "this" points to obj2
