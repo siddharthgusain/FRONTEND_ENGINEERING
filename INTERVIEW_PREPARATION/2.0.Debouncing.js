f0be9e929a5daf1e7984613cace28b87e93cab09
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-20 19:16:38 +0530

    debouncing

--- INTERVIEW_PREPARATION/2.0.Debouncing.js
+++ INTERVIEW_PREPARATION/2.0.Debouncing.js
@@ -3,23 +3,28 @@
 
 const inputObject = document.getElementById("searchBar");
 
-const getData = () => {
+const getData = async () => {
     console.log(this);
-    console.log("SEARCHING.......")
+    console.log("SEARCHING.......");
+    let data = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    data = await data.json();
+    console.log("DATA:", data);
 }
 
 const delayedFunction = function (fun, delay) {
     let timer;
+    console.log("DELAYED FUNCTION CALLED");
     return function (...args) {
+        console.log("DEBOUNCE CALLED");
         clearTimeout(timer);
-        timer = setTimeout(() => {
+        timer = setTimeout(() => { // here closure is made with timer to remember previous timer value
             fun.apply(this, args); // we used apply for obj binding
         }, delay);
     }
 }
 
 /*
- The returned timeoutID is a positive integer value which identifies 
+The returned timeoutID is a positive integer value which identifies 
 the timer created by the call to setTimeout(). This value can be passed 
 to clearTimeout() to cancel the timeout.
 */
@@ -35,3 +40,4 @@ two keystrokes is more than 300ms
 
 
 // can be used for scroll events,infinite scrolling , and window resize
+// Three concepts are used closure , setTimeout and higher order function
