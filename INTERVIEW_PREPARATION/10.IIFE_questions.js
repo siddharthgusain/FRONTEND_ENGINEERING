699d49c4b9e32b1145efaa07e107eaf8d17ecee1
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-22 03:35:46 +0530

    IIFE QUESTIONS

--- INTERVIEW_PREPARATION/10.IIFE_questions.js
+++ INTERVIEW_PREPARATION/10.IIFE_questions.js
@@ -1,6 +1,20 @@
 var a = 1;
 (function () {
-    console.log(a);
+    console.log(a); // will access local "a" first , scope chaining is used
     var a = 2;
     console.log(a);
 })()
+
+console.log(a); // can't access "a" INSIDE IIFE so will access "a=1"
+
+/*
+OUTPUT: 
+undefined
+2
+1
+*/
+let outer = 1;
+(function () {
+    console.log(outer); // will access outer present in global scopes because of scope chain
+})()
+
