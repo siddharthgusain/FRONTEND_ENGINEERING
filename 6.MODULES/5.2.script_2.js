2f016685c3e74cbf67d82ad370cba22edcc71d86
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-26 02:16:46 +0530

    JS modules basics and module bundlers

---
+++ 6.MODULES/5.2.script_2.js
@@ -0,0 +1,8 @@
+a = 20; // we are simply modifying "a" defined in script1, this can be solved with module
+b = 290;
+console.log(a);
+console.log(b)
+
+// NOTE:- both script 1 and script 2 are sharing same "JS ENGINE instance" or same renderer process
+
+// Note theres no import statement for a and b , because these are normal scripts not "modules"
