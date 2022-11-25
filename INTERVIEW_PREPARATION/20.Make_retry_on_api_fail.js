558e426fc764e841181969e28a3d136cecfeb142
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-25 23:11:25 +0530

    API call with max retyr functionaality

--- INTERVIEW_PREPARATION/20.Make_retry_on_api_fail.js
+++ INTERVIEW_PREPARATION/20.Make_retry_on_api_fail.js
@@ -5,5 +5,41 @@ json file. Add a retry logic if the request fails for some reason.
 fetch('https://jsonplaceholder.typicode.com/todos/1')
 */
 
+function withRetryHandling(callback, {
+    baseDelay = 400,
+    logger = console,
+    numberOfTries = 3,
+} = {}) {
 
+    return function callbackWithRetryHandling(...params) {
+        const retry = async (attempt = 1) => {
+            try {
+                return await callback(...params);
+            } catch (error) {
+                if (attempt >= numberOfTries) throw error;
+
+                // Use an increasing delay to prevent flodding the
+                // server with requests in case of a short downtime.
+                const delay = baseDelay * attempt;
+
+                if (logger) logger.warn('Retry because of', error);
+
+                const promise = new Promise(
+                    resolve =>
+                        setTimeout(() => resolve(retry(attempt + 1)), delay)
+                );
+
+                return promise;
+            }
+        };
+
+        return retry();
+    };
+}
+
+const retry = withRetryHandling(() => {
+    return fetch('https://jsonplaceholder.typicode.com/todos/1')
+});
+
+retry().then(res => res.json()).then((data) => console.log(data));
 
