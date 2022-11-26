63b91566c672c9434403e2d948f35afc0362e615
siddharthgusain <siddharthgusain26011999@gmail.com>
2022-11-26 18:28:53 +0530

    MODAL WITH VANILLA JS

---
+++ INTERVIEW_PREPARATION/4.VANILLA_JS_PRACTICE/4.MODAL/script.js
@@ -0,0 +1,23 @@
+const openBtn = document.querySelector(".openBtn");
+const closeBtn = document.querySelector(".closebtn");
+const modalOverlay = document.querySelector(".modalOverlay");
+
+let isModalOpen = false;
+
+function changeModalState() {
+    console.log("clicked");
+    isModalOpen = !isModalOpen;
+    if (isModalOpen) {
+        modalOverlay.classList.add("openModal");
+    } else {
+        modalOverlay.classList.remove("openModal");
+    }
+}
+
+openBtn.addEventListener("click", function () {
+    changeModalState(); // creates closure with outer environment
+});
+
+closeBtn.addEventListener("click", function () {
+    changeModalState(); // creates closure with outer environment
+});
