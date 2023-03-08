b98baa7ed9f4ddc7593687b948d3797ee7f4c6f1
siddharthgusain <siddharthgusain26011999@gmail.com>
2023-03-08 19:36:04 +0530

    Module basics

--- 6.MODULES_AND_PACKAGES/MODULE_WORKING_EXAMPLE/Module.js
+++ 6.MODULES_AND_PACKAGES/MODULE_WORKING_EXAMPLE/Module.js
@@ -1,3 +1,5 @@
+let i = 20;
 export function exportedFunction() {
+    console.log(i);
     console.log("Hi i am exported from another module");
 }
--- 6.MODULES_AND_PACKAGES/MODULE_WORKING_EXAMPLE/main.js
+++ 6.MODULES_AND_PACKAGES/MODULE_WORKING_EXAMPLE/main.js
@@ -5,5 +5,6 @@ import { exportedFunction } from "./Module.js";
 // }
 
 exportedFunction();
+console.log(i); // "i" is not exported from Module.js so not accessible here because of Module scope.
 
 //NOTE:- We can't redeclare same function that was in module
